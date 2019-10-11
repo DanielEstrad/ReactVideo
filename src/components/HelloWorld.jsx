fcd36c821cdaf134bd1d0338a6f7bd2aa0083526
DanielEstrad <163207@ids.upchiapas.edu.mx>
2019-10-11 13:42:21 -0500

    ESLint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+class HelloWorld extends React.Component {
+    render() {
+        return(
+            <div>Click me</div>
+        )
+    }
+}
+
+export default HelloWorld
